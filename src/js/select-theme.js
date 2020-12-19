147c2f7525f0496abe5e9f364936a3219b86e36b
Sashlemekhov <67118539+Sashlemekhov@users.noreply.github.com>
2020-12-19 21:17:37 +0200

    optimized code

--- src/js/select-theme.js
+++ src/js/select-theme.js
@@ -3,38 +3,40 @@ const themeToggleRef = document.querySelector('#theme-switch-toggle');
 
 themeToggleRef.addEventListener('change', themeToggleHandler);
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
 savedThemeHandler();
 
 
 function setDarkThemeHandler() {
-  bodyRef.classList.add('dark-theme');
-  bodyRef.classList.remove('light-theme');
+  bodyRef.classList.add(Theme.DARK);
+  bodyRef.classList.remove(Theme.LIGHT);
 };
 
 function setLightThemeHandler() {
-  bodyRef.classList.add('light-theme');
-  bodyRef.classList.remove('dark-theme');
+  bodyRef.classList.add(Theme.LIGHT);
+  bodyRef.classList.remove(Theme.DARK);
 };
 
 function themeToggleHandler() {
   if (this.checked) {
     setDarkThemeHandler();
-    localStorage.setItem('Current-Theme', 'dark');
+    localStorage.setItem('Current-Theme', Theme.DARK);
   } else {
     setLightThemeHandler();
-    localStorage.setItem('Current-Theme', 'light');
+    localStorage.setItem('Current-Theme', Theme.LIGHT);
   };
 };
 
 function savedThemeHandler() {
   const savedTheme = localStorage.getItem('Current-Theme');
-  if (savedTheme === "dark") {
+  if (savedTheme === Theme.DARK) {
     setDarkThemeHandler();
     themeToggleRef.checked = true;
-  };
-  if (savedTheme === "light") {
-    setLightThemeHandler();
-  };
+  } else setLightThemeHandler();
 };
 
 
